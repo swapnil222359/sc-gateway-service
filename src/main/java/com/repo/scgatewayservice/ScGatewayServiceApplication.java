7b4e80ae46f4b61c06ab894c27d3c130a4f4429e
swapnil222359 <swapsingale@gmail.com>
2018-01-14 16:05:59 +0530

    intial

---
+++ src/main/java/com/repo/scgatewayservice/ScGatewayServiceApplication.java
@@ -0,0 +1,12 @@
+package com.repo.scgatewayservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScGatewayServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ScGatewayServiceApplication.class, args);
+	}
+}
---
+++ src/test/java/com/repo/scgatewayservice/ScGatewayServiceApplicationTests.java
@@ -0,0 +1,16 @@
+package com.repo.scgatewayservice;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ScGatewayServiceApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
